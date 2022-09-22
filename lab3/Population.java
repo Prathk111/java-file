ef788871af09e368c65e6b45f8a369cd00d87ccf
Prathk111 <112954983+Prathk111@users.noreply.github.com>
2022-09-22 18:07:19 +0530

    Add files via upload

---
+++ lab3/Population.java
@@ -0,0 +1,16 @@
+public class Population {
+	public static void main(String[] args)
+	{
+		
+				float a=175000;
+				float b=262500;
+				float sub=b-a;
+				float d=sub/a;
+				float total=d*100;
+				float totalper=total/10;
+				System.out.println("the average percent increase of population per year : "+ totalper+" %");
+			}
+		
+	
+
+}
